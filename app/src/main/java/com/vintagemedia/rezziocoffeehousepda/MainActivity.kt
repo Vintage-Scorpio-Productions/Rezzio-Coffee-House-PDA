f5bc63b64a96374e5671fdd1f75b46a38c4a90d1
borkusgod <lightraven.bourque@gmail.com>
2023-01-05 15:54:22 -0400

    Set up develop branch and protected main

--- app/src/main/java/com/vintagemedia/rezziocoffeehousepda/MainActivity.kt
+++ app/src/main/java/com/vintagemedia/rezziocoffeehousepda/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    // Setting up the develop branch and protecting main
                 }
             }
         }
