976ac73f812e826789438426664357914d432574
borkusgod <lightraven.bourque@gmail.com>
2023-01-08 14:24:55 -0400

    Just getting more things in place so that I can start adding features

--- app/src/main/java/com/vintagemedia/rezziocoffeehousepda/MainActivity.kt
+++ app/src/main/java/com/vintagemedia/rezziocoffeehousepda/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.vintagemedia.rezziocoffeehousepda
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vintagemedia.rezziocoffeehousepda.ui.theme.RezzioCoffeeHousePDATheme
 
 class MainActivity : ComponentActivity() {
@@ -21,9 +31,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // Setting up the develop branch and protecting main
+
+                    // below is for defaulting to landscape
+                    val activity = (LocalContext.current as Activity)
+                    activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                    DefaultPlaceholder()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun DefaultPlaceholder() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+            .padding(1.dp)
+            .border(5.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(5))
+        ,
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(5.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Placeholder")
         }
     }
 }
